0c6f3cd506d0f30b2010fbfd820398722b9e14ce
Lenovo <Lenovo@DESKTOP-HQH9Q0V>
2024-08-05 16:00:09 -0400

    A

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        // Crear un arreglo para almacenar los 10 números
+       
         int[] numeros = new int[10];
 
         // Leer los 10 números desde la consola
@@ -15,10 +15,9 @@ class Program
             numeros[i] = Convert.ToInt32(Console.ReadLine());
         }
 
-        // Ordenar el arreglo
         Array.Sort(numeros);
 
-        // Mostrar los números ordenados
+       
         Console.WriteLine("\nNúmeros ordenados:");
         for (int i = 0; i < numeros.Length; i++)
         {

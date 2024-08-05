b7ee2352c4e5a072aa0f1df73c454d58922c116d
Lenovo <Lenovo@DESKTOP-HQH9Q0V>
2024-08-05 15:54:19 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Crear un arreglo para almacenar los 10 números
+        int[] numeros = new int[10];
+
+        // Leer los 10 números desde la consola
+        Console.WriteLine("Ingrese 10 números:");
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Ordenar el arreglo
+        Array.Sort(numeros);
+
+        // Mostrar los números ordenados
+        Console.WriteLine("\nNúmeros ordenados:");
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine(numeros[i]);
+        }
+    }
+}
+
